7a40c3b43f288e890933b7960e93dd7a56f61470
enami tsugutomo <t.enami@sonyged.com>
2016-08-31 15:04:13 +0900

    Introduce command timeout and drop expired command.

--- device_proxy.js
+++ device_proxy.js
@@ -35,14 +35,14 @@ function Server(opts)
       });
     },
     open: (reply, arg) => {
-      debug(`device-request: ${arg.request}`, arg.arg.device);
+      //debug(`device-request: ${arg.request}`, arg.arg.device);
       device.open(arg.arg.device, err => { reply(null, err); });
     },
     action: (reply, arg) => {
       let action = device.action();
       if (action) {
         action.action(arg.arg.block, arg.arg.arg, err => {
-          debug(`device-request: reply err = ${err}`, arg);
+          //debug(`device-request: reply err = ${err}`, arg);
           reply(err, err);
         });
       } else {
@@ -67,7 +67,7 @@ function Server(opts)
       device.serial_open(err => { reply(null, err); });
     },
     serial_write: (reply, arg) => {
-      debug(`device-request: ${arg.request}`, arg.arg.data);
+      //debug(`device-request: ${arg.request}`, arg.arg.data);
       device.serial_write(arg.arg.data, err => { reply(null, err); });
     },
     serial_event: (reply, arg, notify) => {
@@ -93,21 +93,21 @@ function Server(opts)
     }
   };
   listener('device-request', (sender, arg) => {
-    debug('device-request', arg);
+    //debug('listener: device-request', arg);
     const reply = (response, error) => {
-      debug('device-request: reply', arg, response);
+      //debug('listener: device-request: reply', arg, response);
       sender('device-reply', {
         request: arg.request, arg: response, error: error, id: arg.id
       });
     };
     const notify = (notification) => {
-      debug('device-notify', arg, notification);
+      //debug('device-notify', arg, notification);
       sender('device-notify', {
         request: arg.request, arg: notification, id: arg.id
       });
     };
     let handler = this.handler[arg.request];
-    debug('device-request: handler', handler);
+    //debug('device-request: handler', handler);
     if (handler)
       handler(reply, arg, notify);
     else
@@ -119,24 +119,67 @@ function Client(opts)
 {
   let sender = opts.sender;
   let listener = opts.listener;
+  let command_timeout = 60000;
   if (opts.debug)
     debug = opts.debug;
+  if (opts.command_timeout)
+    command_timeout = opts.command_timeout;
 
   this.cmdid = 0;
   this.current_cmd = null;
+  this.timeout_id = null;
   this.cmdq = [];
-  this.cmdq_pending = [];
   this.notifier = {};
   this.serial_events = {};
+
+  const flush_cmdq = () => {
+    while (true) {
+      const cmd = this.cmdq.pop();
+      if (!cmd)
+        return;
+      debug('flusing cmd:', cmd.id);
+      cmd.cb();
+    }
+  };
+
+  const drop_cmd = (id) => {
+    this.cmdq = this.cmdq.filter(x => { return x.id !== id; });
+  };
+
+  const drop_expired = (cmd) => {
+    this.timeout_id = null;
+    if (!this.current_cmd) {
+      debug('drop_expired: no current command');
+      return;
+    }
+    if (cmd.id !== this.current_cmd.id) {
+      debug('drop_expired: id mismatch', cmd.id, this.current_cmd.id);
+      return;
+    }
+    this.current_cmd = null;
+    drop_cmd(cmd.id);
+    debug('drop_expired: drop command', cmd.id);
+    setTimeout(send_cmd, 0);
+    return cmd.callback({ msg: 'command timeout' });
+  };
 
   const send_cmd = () => {
     if (this.current_cmd !== null ||
         this.cmdq.length === 0)
       return;
 
-    debug('send_cmd:', this.cmdq);
+    //debug('send_cmd:', this.cmdq);
     const cmd = this.cmdq[0];
+    const now = Date.now();
+    if (now > cmd.timestamp + command_timeout) {
+      debug('send_cmd: drop due to out of date', cmd);
+      this.cmdq.pop();
+      setTimeout(send_cmd, 0);
+      return cmd.callback({ msg: 'command timeout' });
+    }
     this.current_cmd = cmd;
+    this.timeout_id = setTimeout(() => { drop_expired(cmd); }, command_timeout);
+    //debug('send_cmd: dequeue', cmd);
     return sender('device-request', {
       request: cmd.request, arg: cmd.arg, id: cmd.id
     });
@@ -147,20 +190,30 @@ function Client(opts)
   };
   this.request = (type, arg, cb) => {
     const id = this.cmdid++;
-    const cmd = { request: type, arg: arg, callback: cb, id: id };
-    debug('device_proxy: request', cmd);
+    const cmd = {
+      request: type,
+      arg: arg,
+      callback: cb,
+      id: id,
+      timestamp: Date.now()
+    };
+    //debug('device_proxy: request (enqueue)', cmd);
     this.send_cmd(cmd);
   };
   listener('device-reply', (arg) => {
-    debug('device_proxy: reply', arg);
+    //debug('device_proxy: reply', arg);
     let cmd = this.cmdq.find(x => { return x.id === arg.id; });
     if (cmd) {
-      this.cmdq = this.cmdq.filter(x => { return x.id !== arg.id; });
+      drop_cmd(arg.id);
       this.current_cmd = null;
+      if (this.timeout_id) {
+        clearTimeout(this.timeout_id);
+        this.timeout_id = null;
+      }
       setTimeout(send_cmd, 0);
       return cmd.callback(arg.error || arg.arg);
     } else {
-      debug('device_proxy: reply: no cmd found', arg);
+      debug('device_proxy: reply: no cmd found', arg, this.cmdq);
     }
   });
   listener('device-notify', (arg) => {
@@ -169,10 +222,18 @@ function Client(opts)
     if (notifier)
       notifier(arg.arg);
   });
+
+  this.reset = (cb) => {
+    flush_cmdq();
+    this.current_cmd = null;
+    this.notifier = {};
+    this.serial_events = {};
+    return cb(null);
+  };
   this.action = () => {
     return {
       action: (block, arg, cb) => {
-        debug('device_proxy: action', block);
+        //debug('device_proxy: action', block);
         this.request('action', { block: block, arg: arg }, cb);
       }
     };
@@ -202,13 +263,12 @@ function Client(opts)
     this.request('serial_write', { data: data }, cb);
   };
   this.notifier['serial_event'] = (arg) => {
-    debug('notifier: serial_event', arg);
+    //debug('notifier: serial_event', arg);
     const notify = this.serial_events[arg.what];
     if (notify) {
-      debug('notifer: do notify', notify);
       notify(arg.data);
     } else
-      debug('notifer: not found');
+      debug('notifer: not found', arg);
   };
   this.serial_event = (what, cb, notify) => {
     const event_name = `serial-event:${what}`;
@@ -217,34 +277,18 @@ function Client(opts)
   };
   this.program_serial = () => {
     /*
-     * Write with dividing into 20 byte chunks.
-     */
-    const ble_write = (data, callback) => {
-      let length = data.length;
-      if (length > 20)
-        length = 20;
-      debug('write!', data);
-      this.serial_write(data.slice(0, length), err => {
-        data = data.slice(length);
-        debug('write! complete', data);
-        if (data.length > 0)
-          return ble_write(data, callback);
-        else {
-          debug('write done!');
-          callback();
-        }
-      });
-    };
-    /*
-     * ble serial wrapper for stk500v2 module.
+     * serial wrapper for stk500v2 module.
      */
     const serial = {
       path: 'dummy',        // this is necessary.
       open: (callback) => { return callback(null); },
       on: (what, callback) => { this.serial_event(what, () => {}, callback); },
       close: (callback) => { this.close(callback); },
-      write: ble_write,
-      drain: (callback) => { debug('drain called'); callback(null); },
+      write: (data, callback) => { return this.serial_write(data, callback); },
+      drain: (callback) => {
+        //debug('drain called');
+        callback(null);
+      },
     };
     return serial;
   };
